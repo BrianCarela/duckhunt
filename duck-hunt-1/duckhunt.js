028d1e8fb59c8941284eb3a29a32a628c3ea62d0
Brian Carela <BrianCarela@gmail.com>
2018-09-05 12:26:22 -0400

    ready for use

--- duck-hunt-1/duckhunt.js
+++ duck-hunt-1/duckhunt.js
@@ -6,33 +6,22 @@ jQuery(function() { //less characters than the $(document).ready(function){})
   // I'll do some of the work for you, first I will grab the <body></body>
   var $body = $('body');
 
-  // 1. Can you create a <div> with the class "duck" and name it "duck"
+  // Create a <div> with the class "duck" and name it "duck"
   var $duck = $('<div>').addClass('duck').appendTo($body);
 
-  // 2. Next, use setInterval to toggle the "flap" class on the duck every 250 ms (1/4 second)
+  // 1. Next, use setInterval to toggle the "flap" class on the duck every 250 ms (1/4 second)
   setInterval(function(){
-    $duck.toggleClass('flap');
+    // Hint: use the toggleClass() method in jquery!
   }, 250);
 
-  // 3. Fantastic!  Now, let's move the duck using CSS "top" and "left"
-  $duck.css({
-    top: '100px',
-    left: '100px'
-  });
+  // 2. Fantastic!  Now, let's move the duck using CSS "top" and "left"
+  // Hint: use the .css() method in jQuery!
 
-  // 4. Try making the duck move to a different location after 1 second
-  //method A
-  // setInterval(function(){
-  //   $duck.css({
-  //       top: '100px',
-  //       left: '100px'
-  //   });
-  // }, 1000);
-  //method B
-  $duck.animate({
-        top: Math.random()*500,
-        left: Math.random()*500
-  }, 1000);
 
-  // 5. Congratulations!
+  // 3. Try making the duck move to a different location after 1 second
+  // Hint: look up the .animate() method in jQuery!
+
+
+
+  // 4. Congratulations!
 });
